62938216be13d15bd63776a64d28bb15c1272d9e
LiaoXP <lxpld47@gmail.com>
2021-03-24 23:59:22 +0800

    regular commit

--- Udemy_Python Programming Bootcamp/97. Binary Trees-Creating, Traversing, Searching & More.py
+++ Udemy_Python Programming Bootcamp/97. Binary Trees-Creating, Traversing, Searching & More.py
@@ -1,22 +1,48 @@
+class Node:
+    def __init__(self, key = 0, name = ""):
+        self.key = key
+        self.name = name
+
+        self.left_child = None
+        self.right_child = None
+
 class BinaryTree:
     def __init__(self):
         self.root = None
 
     def add_node(self, key, name):
         new_node = Node(key, name)
+
         if self.root is None:
             self.root = new_node
         else:
             focus_node = self.root
+
             while True:
-                
+                parent = focus_node
 
-class Node:
-    def __init__(self, key=0, name=""):
-        self.key = key
-        self.name = name
-        self.left_child = None
-        self.right_child = None
+                if key < focus_node.key:
+                    focus_node = focus_node.left_child
 
-    def __str__(self):
-        return f"{self.name} has the key {self.key}"
+                    if focus_node is None:
+                        parent.left_child = new_node
+
+                        return True
+                else:
+                    focus_node = focus_node.right_child
+
+                    if focus_node is None:
+                        parent.right_child = new_node
+
+                        return True
+
+    def in_order_traverse(self, focus_node):
+        if focus_node is not None:
+            self.in_order_traverse(focus_node.left_child)
+            print(focus_node)
+            self.in_order_traverse(focus_node.right_child)
+
+    def preorder_traverse(self, focus_node):
+        if focus_node is not None:
+            print
+
