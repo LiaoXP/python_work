265702d05067534adad484f125adc0ae617826a7
LiaoXP <lxpld47@gmail.com>
2022-03-18 00:37:33 +0800

    regular commit

--- 微信读书 算法图解/4.1 分而治之 divide and conquer.py
+++ 微信读书 算法图解/4.1 分而治之 divide and conquer.py
@@ -8,25 +8,26 @@
 #         return sum(arr[0:len(arr) - 1]) + arr[len(arr) - 1]
 # print(sum(range(1,101)))
 
-# #4.2 编写一个递归函数来计算列表包含的元素数。
-# print(len([2,4,6]))
-# def listQty(lst):
-#     if lst[0] == None:
-#         return 0
-#     else:
-#         return listQty(lst.pop()) + 1
-# print(listQty([2,4,6]))
-
-#4.3 找出列表中最大的数字。
-def find_the_biggest(lst):
-    if len(lst) == 0:
+#4.2 编写一个递归函数来计算列表包含的元素数。
+print(len([2,4,6]))
+def listQty(lst):
+    if lst == []:
         return 0
-    elif len(lst) == 1:
-        return lst[0]
     else:
-        biggest = lst[len(lst) - 1]
-        if biggest >= find_the_biggest(lst[0:len(lst) - 1]):
-            return biggest
-        else:
-            return find_the_biggest(lst[0:len(lst) - 1])
-print(find_the_biggest([8,5,2]))
+        lst.pop()
+        return listQty(lst) + 1
+print(listQty([2,4,6]))
+
+# #4.3 找出列表中最大的数字。
+# def find_the_biggest(lst):
+#     if len(lst) == 0:
+#         return 0
+#     elif len(lst) == 1:
+#         return lst[0]
+#     else:
+#         biggest = lst[len(lst) - 1]
+#         if biggest >= find_the_biggest(lst[0:len(lst) - 1]):
+#             return biggest
+#         else:
+#             return find_the_biggest(lst[0:len(lst) - 1])
+# print(find_the_biggest([8,5,2]))
